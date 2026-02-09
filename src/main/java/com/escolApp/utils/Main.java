703b6c67c9129b1ee366f4adf04023352256bb76
JobasStuff <jparaujo0409@gmail.com>
2026-02-09 09:00:58 -0300

    arrumando os diretorios

--- src/main/java/com/escolApp/utils/Main.java
+++ src/main/java/com/escolApp/utils/Main.java
@@ -5,15 +5,15 @@ import com.escolApp.conexao.Conexao;
 public class Main {
     public static void main(String[] args) {
         System.out.println(System.getenv("DB_URL"));
-//        Conexao con1 = new Conexao();
-//        if(con1.conectar() == null){
-//            System.out.println("Não conectou");
-//            System.out.println(con1.conectar());
-//        }
-//        else {
-//            System.out.println("Conectou");
-//            System.out.println(con1.conectar());
-//        }
-//        con1.desconectar(con1.conectar());
+        Conexao con1 = new Conexao();
+        if(con1.conectar() == null){
+            System.out.println("Não conectou");
+            System.out.println(con1.conectar());
+        }
+        else {
+            System.out.println("Conectou");
+            System.out.println(con1.conectar());
+        }
+        con1.desconectar(con1.conectar());
     }
 }
